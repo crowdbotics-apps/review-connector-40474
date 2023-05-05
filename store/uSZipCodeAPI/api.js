4867a411dde22092b23468c33fb7306a18baaa47
Crowdbotics <engineering@crowdbotics.com>
2023-05-05 08:51:55 +0000

    Update store.

---
+++ store/uSZipCodeAPI/api.js
@@ -0,0 +1,14 @@
+import axios from "axios"
+import { US_ZIP_CODE_API_SUPPORT_TOKEN } from "react-native-dotenv"
+const uSZipCodeAPI = axios.create({
+  baseURL: "https://us-zip-code-information.p.rapidapi.com",
+  headers: {
+    Accept: "application/json",
+    "Content-Type": "application/json",
+    Authorization: `Bearer ${US_ZIP_CODE_API_SUPPORT_TOKEN}`
+  }
+})
+function uszipcodeapi_get__read(payload) {
+  return uSZipCodeAPI.get(`/`, { params: { zipcode: payload.zipcode } })
+}
+export const apiService = { uszipcodeapi_get__read }
---
+++ store/uSZipCodeAPI/uszipcodeapi_response_get_GetZipCodeInfos.slice.js
@@ -0,0 +1,41 @@
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit"
+import { apiService } from "./api"
+export const uszipcodeapi_get__read = createAsyncThunk(
+  "uszipcodeapi_response_get_GetZipCodeInfos/uszipcodeapi_get__read",
+  async payload => {
+    const response = await apiService.uszipcodeapi_get__read(payload)
+    return response.data
+  }
+)
+const initialState = { entities: [], api: { loading: "idle", error: null } }
+const uszipcodeapi_response_get_GetZipCodeInfosSlice = createSlice({
+  name: "uszipcodeapi_response_get_GetZipCodeInfos",
+  initialState,
+  reducers: {},
+  extraReducers: {
+    [uszipcodeapi_get__read.pending]: (state, action) => {
+      if (state.api.loading === "idle") {
+        state.api.loading = "pending"
+      }
+    },
+    [uszipcodeapi_get__read.fulfilled]: (state, action) => {
+      if (state.api.loading === "pending") {
+        state.entities = [
+          ...state.entities.filter(record => record.id !== action.payload.id),
+          action.payload
+        ]
+        state.api.loading = "idle"
+      }
+    },
+    [uszipcodeapi_get__read.rejected]: (state, action) => {
+      if (state.api.loading === "pending") {
+        state.api.error = action.error
+        state.api.loading = "idle"
+      }
+    }
+  }
+})
+export default {
+  uszipcodeapi_get__read,
+  slice: uszipcodeapi_response_get_GetZipCodeInfosSlice
+}
